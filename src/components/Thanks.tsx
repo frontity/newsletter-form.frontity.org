f458683c6fe09528d117d2167f656dd2f4c0a2f8
David Arenas <darensr@gmail.com>
2019-03-19 19:20:45 +0100

    feat(thanks): add texts

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Heading, Text, Button } from "grommet";
+import { Github, Twitter } from "grommet-icons";
+import Frontity from "./Icon";
 
 const Thanks = () => (
   <Box
@@ -11,10 +13,44 @@ const Thanks = () => (
     background="white"
   >
     <Box gap="10px">
-      <Heading margin="0">Big Thanks!</Heading>
-      <Text>We will send you an email when the alpha is ready 💪🤗</Text>
+      <Heading level="2" margin="0">
+        Many thanks for taking the time to answer!
+      </Heading>
+      <Text>
+        You’ll be the first to hear about our news and updates, including the
+        alpha version. 💪🤗
+      </Text>
+      <Text>Here’s what you can do next:</Text>
     </Box>
-    <Button primary color="brand" label="Join our community" />
+    <Box direction="column" gap="20px">
+      <Box direction="row" gap="medium">
+        <Box flex={false} width="60px" height="60px" align="center" justify="center" round="full" background="#f7f7f7">
+          <Github color="#444444" size="40px" />
+        </Box>
+        <Box gap="small">
+          <Heading level="3" margin="0">Learn more</Heading>
+          <Text>Take a look at our Github repository.</Text>
+        </Box>
+      </Box>
+      <Box direction="row" gap="medium">
+        <Box flex={false} width="60px" height="60px" align="center" justify="center" round="full" background="#f7f7f7">
+          <Frontity size={30} />
+        </Box>
+        <Box gap="small">
+          <Heading level="3" margin="0">Get involved</Heading>
+          <Text>come say hello to the forum, introduce yourself or ask any questions!</Text>
+        </Box>
+      </Box>
+      <Box direction="row" gap="medium">
+        <Box flex={false} width="60px" height="60px" align="center" justify="center" round="full" background="#f7f7f7">
+          <Twitter color="#444444" size="32px" />
+        </Box>
+        <Box gap="small">
+          <Heading level="3" margin="0">Tweet about us</Heading>
+          <Text>Help us spread the word!</Text>
+        </Box>
+      </Box>
+    </Box>
   </Box>
 );
 
